949700d407d266ccedea3c64981a8099a7b91920
Dylan <dylan4047@gmail.com>
2017-01-31 16:10:48 +0100

    affichage des chaussete bleu de pointure superieur a 40

--- index_pdo.php
+++ index_pdo.php
@@ -7,7 +7,41 @@ $bdd = new PDO(
 
 $response = $bdd->query('SELECT * FROM mes_chaussettes');
 ?>
+<!-- TOUTE LES DONNEES -->
+<table>
+<h3>TOUTE LES DONNEES</h3>
+	<tr>
+		<th>id</th>
+		<th>pointure</th>
+		<th>temp_lavage</th>
+		<th>description </th>
+		<th>couleur</th>
+		<th>date_lavage</th>
+	</tr>
+		<?php
+		while ($donnees = $response->fetch()) {?>
+	<tr>
+		<td><?= $donnees["id"] ?></td>
+		<td><?= $donnees["pointure"] ?></td>
+		<td><?= $donnees["temp_lavage"] ?></td>
+		<td><?= $donnees["description"]?></td>
+		<td><?= $donnees["couleur"]?></td>
+		<td><?= $donnees["date_lavage"]?></td>
+	</tr>		
 
+<?php }?>
+</table>
+<?php 
+$response->closeCursor(); 
+?>
+
+<!-- chaussettes bleues de pointure supérieure à 40 -->
+
+<?php 
+
+$response2 = $bdd->query('SELECT * FROM mes_chaussettes WHERE couleur="bleu" AND pointure>40;');
+?>
+<h3>chaussettes bleues de pointure supérieure à 40</h3>
 <table>
 	<tr>
 		<th>id</th>
@@ -18,21 +52,18 @@ $response = $bdd->query('SELECT * FROM mes_chaussettes');
 		<th>date_lavage</th>
 	</tr>
 		<?php
-		while ($données = $response->fetch()) {?>
+		while ($donnees2 = $response2->fetch()) {?>
 	<tr>
-		<td><?= $données["id"] ?></td>
-		<td><?= $données["pointure"] ?></td>
-		<td><?= $données["temp_lavage"] ?></td>
-		<td><?= $données["description"]?></td>
-		<td><?= $données["couleur"]?></td>
-		<td><?= $données["date_lavage"]?></td>
+		<td><?= $donnees2["id"] ?></td>
+		<td><?= $donnees2["pointure"] ?></td>
+		<td><?= $donnees2["temp_lavage"] ?></td>
+		<td><?= $donnees2["description"]?></td>
+		<td><?= $donnees2["couleur"]?></td>
+		<td><?= $donnees2["date_lavage"]?></td>
 	</tr>		
 
-<?php }?>
-</table>
-<?php 
-$response->closeCursor(); 
+<?php }
+$response2->closeCursor(); 
 ?>
-
-
+</table>
 
